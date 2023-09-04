b58eb923e5dae84d91a50b379117184ff962cd2f
Andreas Öhlund <andreas.ohlund@particular.net>
2023-09-04 13:48:08 +0200

    Bump to core 9

--- src/NServiceBus.Metrics.PerformanceCounters.Tests/APIApprovals.cs
+++ src/NServiceBus.Metrics.PerformanceCounters.Tests/APIApprovals.cs
@@ -18,15 +18,8 @@
                 "System.Reflection.AssemblyMetadataAttribute"
             }
             });
-#if NET7_0
-            Approver.Verify(publicApi, scenario: "net7.0");
-#elif NET6_0
-            Approver.Verify(publicApi, scenario: "net6.0");
-#elif NET472
-            Approver.Verify(publicApi, scenario: "net472");
-#else
-        throw new Exception("Unknown target framework in API Approval test");
-#endif
+
+            Approver.Verify(publicApi);
         }
     }
 }
